89f2a5c485d0444fd41ab805fe559d0bd7629f54
wangliangliang <wangliangliang.sdc@sdc.com>
2018-01-23 15:14:21 +0800

    修改移动设备登录跳转到m目录下

--- js/index.js
+++ js/index.js
@@ -4,70 +4,73 @@
 
  */
 
+//判断当前设备是否是pc端
+var checkUserAgentInfo = function(){
+  function IsPC() {
+    var userAgentInfo = navigator.userAgent;
+    var Agents = ["Android", "iPhone",
+                "SymbianOS", "Windows Phone",
+                "iPad", "iPod"];
+    var flag = true;
+   for (var v = 0; v < Agents.length; v++) {
+        if (userAgentInfo.indexOf(Agents[v]) > 0) {
+            flag = false;
+           break;
+       }
+   }
+   return flag;
+  }
+
+
+  var pcflag = IsPC()
+  
+  if(pcflag == false){
+      var str=window.location.href;
+      var pattern = "/index.html";
+      str = str.replace(new RegExp(pattern), "");
+      window.location.href= str+ '/m/index.html'
+  }
+}
+
+checkUserAgentInfo();
+
 window.onload = function(){
-        //判断当前设备是否是pc端
-        function IsPC() {
-          var userAgentInfo = navigator.userAgent;
-          var Agents = ["Android", "iPhone",
-                      "SymbianOS", "Windows Phone",
-                      "iPad", "iPod"];
-          var flag = true;
-         for (var v = 0; v < Agents.length; v++) {
-              if (userAgentInfo.indexOf(Agents[v]) > 0) {
-                  flag = false;
-                 break;
-             }
-         }
-         return flag;
-         console.log(111)
-        }
+              
+  //点击回到顶部
+  var oTop = document.getElementById("to_top");
+  oTop.onclick = function(){
 
- 
-        var pcflag = IsPC()
-        
-        if(pcflag == false){
-            var str=window.location.href;
-            var pattern = "/index.html";
-            str = str.replace(new RegExp(pattern), "");
-            window.location.href= str+ '/m/index.html'
-            
-        }
+      document.body.scrollTop = document.documentElement.scrollTop = 0;
+  }
 
-        //点击回到顶部
-        var oTop = document.getElementById("to_top");
-        oTop.onclick = function(){
+  //全屏轮播
+  var newSlideSize = function slideSize(){
+      var w = Math.ceil($(".swiper-container").width()/3/*--调整高度---*/);
+      $(".swiper-container,.swiper-wrapper,.swiper-slide").height(w);
+  };
 
-            document.body.scrollTop = document.documentElement.scrollTop = 0;
-        }
+  newSlideSize();
 
-        //全屏轮播
-        var newSlideSize = function slideSize(){
-            var w = Math.ceil($(".swiper-container").width()/3/*--调整高度---*/);
-            $(".swiper-container,.swiper-wrapper,.swiper-slide").height(w);
-        };
+  $(window).resize(function(){
+      newSlideSize();
+  });
 
-        newSlideSize();
+  var mySwiper = new Swiper('.swiper-container',{
+      pagination: '.pagination',
+      loop:true,
+      autoplay:4000,
+      paginationClickable: true,
+      onSlideChangeStart: function(){
+          //回调函数
+      }
 
-        $(window).resize(function(){
-            newSlideSize();
-        });
+  });
 
-        var mySwiper = new Swiper('.swiper-container',{
-            pagination: '.pagination',
-            loop:true,
-            autoplay:4000,
-            paginationClickable: true,
-            onSlideChangeStart: function(){
-                //回调函数
-            }
+  //导航栏点击跳转
+  $('.gigamenu li').click(function(){
+      $(this).addClass('active').siblings().removeClass('active');
+  })
 
-        });
-
-        //导航栏点击跳转
-        $('.gigamenu li').click(function(){
-            $(this).addClass('active').siblings().removeClass('active');
-        })
-    
 
 
 };
